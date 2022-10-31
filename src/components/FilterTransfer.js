b5ca03b8a30ec4bbf976b6227776e859ee1d3b38
Veronika Paist <nika.kuleva@yandex.ru>
2022-10-31 18:16:48 +0300

    feat: customized checkbox

--- src/components/FilterTransfer.js
+++ src/components/FilterTransfer.js
@@ -3,40 +3,40 @@ import FilterBarVertical from "./FilterBarVertical";
 export default function FilterTransfer() {
   return (
     <FilterBarVertical text="КОЛИЧЕСТВО ПЕРЕСАДОК">
-      <label className="form__label">
+      <label className="form__label from__label_place_filter-transfer">
         <input
           type="checkbox"
-          className="form__checkbox"
+          className="form__checkbox form__checkbox_place_filter-transfer"
           id="without-transfer"
         ></input>
-        <span className="form__customized-checkbox"></span>
+        <span className="form__customized-checkbox form__customized-checkbox_place_filter-transfer"></span>
         <span className="form__text">без пересадки</span>
       </label>
-      <label className="form__label">
+      <label className="form__label from__label_place_filter-transfer">
         <input
           type="checkbox"
-          className="form__checkbox"
+          className="form__checkbox form__checkbox_place_filter-transfer"
           id="one-transfer"
         ></input>
-        <span className="form__customized-checkbox"></span>
+        <span className="form__customized-checkbox form__customized-checkbox_place_filter-transfer"></span>
         <span className="form__text">1 пересадка</span>
       </label>
-      <label className="form__label">
+      <label className="form__label from__label_place_filter-transfer">
         <input
           type="checkbox"
-          className="form__checkbox"
+          className="form__checkbox form__checkbox_place_filter-transfer"
           id="two-transfer"
         ></input>
-        <span className="form__customized-checkbox"></span>
+        <span className="form__customized-checkbox form__customized-checkbox_place_filter-transfer"></span>
         <span className="form__text">2 пересадки</span>
       </label>
-      <label className="form__label">
+      <label className="form__label from__label_place_filter-transfer">
         <input
           type="checkbox"
-          className="form__checkbox"
+          className="form__checkbox form__checkbox_place_filter-transfer"
           id="three-transfer"
         ></input>
-        <span className="form__customized-checkbox"></span>
+        <span className="form__customized-checkbox form__customized-checkbox_place_filter-transfer"></span>
         <span className="form__text">3 пересадки</span>
       </label>
     </FilterBarVertical>
